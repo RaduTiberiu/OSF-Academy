a9a82d276f9adc6a928cee2a0ed830c7eceb4829
RaduTiberiu <valacu.tiberiu15@yahoo.com>
2021-11-17 17:36:50 +0200

    Finished mobile home page (no links to other pages)

--- scripts/customJS.js
+++ scripts/customJS.js
@@ -4,6 +4,10 @@ let overlayTwo = document.getElementById("overlay-two");
 let itemThree = document.getElementById("item-three");
 let currentDate = document.getElementById("current-date");
 let date = new Date();
+let addCart = document.getElementsByClassName("add-cart");
+let cartCounter = document.getElementById("cart-counter");
+let addFav = document.getElementsByClassName("add-fav");
+let favCounter = document.getElementById("fav-counter");
 
 itemOne.onmouseover = function() {
     overlayOne.style.display = "block";
@@ -22,3 +26,19 @@ itemThree.onmouseout = function() {
 }
 
 currentDate.innerHTML = `&copy; Copyright ${date.getFullYear()}`;
+
+let cartPlus = function () {
+    cartCounter.innerHTML = Number(cartCounter.innerHTML) + 1;
+};
+
+for (var i = 0; i < addCart.length; i++) {
+    addCart[i].addEventListener('click', cartPlus);
+};
+
+let favPlus = function () {
+    favCounter.innerHTML = Number(favCounter.innerHTML) + 1;
+};
+
+for (var i = 0; i < addFav.length; i++) {
+    addFav[i].addEventListener('click', favPlus);
+}
