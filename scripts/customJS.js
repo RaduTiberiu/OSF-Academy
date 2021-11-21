79cc62040c3a33dc660984a361b0fa8759840906
RaduTiberiu <79359792+RaduTiberiu@users.noreply.github.com>
2021-11-21 21:22:08 +0200

    Added category landing page

--- scripts/customJS.js
+++ scripts/customJS.js
@@ -3,11 +3,13 @@ let itemOne = document.getElementById("item-one");
 let overlayTwo = document.getElementById("overlay-two");
 let itemThree = document.getElementById("item-three");
 let currentDate = document.getElementById("current-date");
-let date = new Date();
 let addCart = document.getElementsByClassName("add-cart");
 let cartCounter = document.getElementById("cart-counter");
 let addFav = document.getElementsByClassName("add-fav");
 let favCounter = document.getElementById("fav-counter");
+let loadMore = document.getElementsByClassName("load-more");
+let hiddenCard = document.getElementsByClassName("hidden-card");
+
 
 itemOne.onmouseover = function() {
     overlayOne.style.display = "block";
@@ -25,7 +27,7 @@ itemThree.onmouseout = function() {
     overlayTwo.style.display = "none";
 }
 
-currentDate.innerHTML = `&copy; Copyright ${date.getFullYear()}`;
+
 
 let cartPlus = function () {
     cartCounter.innerHTML = Number(cartCounter.innerHTML) + 1;
@@ -48,3 +50,13 @@ window.addEventListener('load', function(){
         document.getElementById('modal-button').click();
     }, 2000);
  });
+
+
+ let showProds = function () {
+    for (var i = 0; i < hiddenCard.length; i++) {
+        hiddenCard[i].style.display = "block"
+    };
+    loadMore[0].style.display = "none";
+ };
+
+ loadMore[0].addEventListener('click', showProds);
